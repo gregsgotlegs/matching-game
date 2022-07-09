33da6e1ea1907cc1ba142bdae0f511358f4cda8d
Amanda G. Brown <green6erry@gmail.com>
2022-07-09 18:27:30 -0400

    Created start and end pages for later

--- src/app/play/play-routing.module.ts
+++ src/app/play/play-routing.module.ts
@@ -4,13 +4,28 @@ import { PlayPage } from './play.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'game',
     component: PlayPage,
-  }
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    redirectTo: 'start',
+    pathMatch: 'full',
+  },
+  {
+    path: 'start',
+    loadChildren: () =>
+      import('./start/start.module').then((m) => m.StartPageModule),
+  },
+  {
+    path: 'finish',
+    loadChildren: () => import('./finish/finish.module').then( m => m.FinishPageModule)
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class PlayPageRoutingModule {}
